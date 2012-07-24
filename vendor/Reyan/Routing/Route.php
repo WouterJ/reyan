bec1280cb5bb5b141661a2cf8f95d13384fdd247
WouterJ <waldio.webdesign@gmail.com>
2012-07-24 23:30:43 +0200

    [Routing] Created first match method

--- vendor/Reyan/Routing/Route.php
+++ vendor/Reyan/Routing/Route.php
@@ -21,13 +21,45 @@ class Route
      * @param array  $defaults     Optional The defaults for parameters in the route
      * @param array  $requirements Optional The requirements for the parameter
      */
-    public function __construct($pattern, $action, array $defaults = array(), array $requirements = array(), $method = '')
+    public function __construct($pattern, $action, array $defaults = array(), array $requirements = array())
     {
         $this->name = (string) $name;
         $this->pattern = (string) $pattern;
         $this->action = (string) $action;
         $this->defaults = $defaults;
         $this->requirements = $requirements;
+    }
+
+    /**
+     * Match a URI to this route
+     *
+     * @param  string $uri
+     *
+     * @return boolean
+     */
+    public function match($uri)
+    {
+        if (null === $this->_rgx) {
+            $this->setRegex($this->pattern);
+        }
+
+        if (preg_match($this->_rgx, $uri)) {
+        }
+    }
+
+    /**
+     * Create the regex to match the route
+     *
+     * @access protected
+     */
+    protected function setRegex($pattern)
+    {
+        $this->_rgx = '|';
+        $this->_rgx .= preg_replace('|:(\w*)|', '(?P<$1>.*?)', $pattern);
+        $this->_rgx .= ('/' != substr($pattern, -1)
+                            ? '/'
+                            : ''
+                       ).'|';
     }
 
     public function getPattern() 
