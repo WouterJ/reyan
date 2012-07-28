3ad0a25320326db64d6a7590cf71f66b4de07b52
WouterJ <waldio.webdesign@gmail.com>
2012-07-28 16:26:36 +0200

    [Routing] Created YmlRoutesLoader

---
+++ vendor/Reyan/Framework/Routing/Loader/YmlRoutesLoader.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Reyan\Framework\Routing\Loader;
+
+use Reyan\Framework\Routing\Route;
+use Reyan\Framework\Routing\RouteCollection;
+use Reyan\Framework\Loader\YmlFileLoader;
+
+class YmlRoutesLoader extends YmlFileLoader
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return RouteCollection In which all routes (and other collections) are stored
+     */
+    protected function generate($data)
+    {
+        $collection = new RouteCollection();
+
+        foreach ($data as $name => $options) {
+            if (isset($options['resource'])) {
+                // resource
+                $route = $this->load($options['resource']);
+            } else {
+                // route
+                if (!isset($options['defaults'])) {
+                    $options['defaults'] = array();
+                }
+                if (!isset($options['requirements'])) {
+                    $options['requirements'] = array();
+                }
+                if (!isset($options['method'])) {
+                    $options['method'] = 'ALL';
+                }
+
+                $route = new Route($options['pattern'], $options['controller'], $options['defaults'], $options['requirements'], $options['method']);
+            }
+
+            $collection->add($route);
+        }
+
+        return $collection;
+    }
+}
---
+++ vendor/Reyan/Framework/Tests/Routing/Loader/YmlRoutesLoaderTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Reyan\Framework\Tests\Routing\Loader;
+
+use \PHPUnit_Framework_TestCase as TC;
+
+class YmlRoutesLoaderTest extends TC
+{
+}
